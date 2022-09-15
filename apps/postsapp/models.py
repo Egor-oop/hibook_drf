f036b4a9ffa3bffd12835b26aa0bf8db2853b1cb
Egor-oop <gulidoegor@gmail.com>
2022-09-15 21:43:31 +0300

    feat: Post model

--- apps/postsapp/models.py
+++ apps/postsapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from apps.accountsapp.models import CustomUser
+
+
+class Post(models.Model):
+    """Post model"""
+    text = models.TextField(max_length=512)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
