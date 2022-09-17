c4f073ea1309706977ec2cbc43800baca898b35f
Egor-oop <gulidoegor@gmail.com>
2022-09-17 12:59:49 +0300

    chores: updated user model

--- apps/postsapp/models.py
+++ apps/postsapp/models.py
@@ -5,6 +5,7 @@ from apps.accountsapp.models import CustomUser
 
 class Post(models.Model):
     """Post model"""
+    
     text = models.TextField(max_length=512, blank=False, null=False)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=True, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
