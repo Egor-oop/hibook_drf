21a4dc392403fe18df54a392287ce190159ee93c
Egor-oop <gulidoegor@gmail.com>
2022-09-17 13:24:01 +0300

    feat: only owner of post can post it

--- apps/postsapp/views.py
+++ apps/postsapp/views.py
@@ -2,11 +2,13 @@ from rest_framework import viewsets
 
 from .models import Post
 from .serializers import PostSerializer
+from apps.accountsapp.permissions import IsOwnerOrReadOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
