35a17b3ec96c8bdbe534bbdf9cd239dbb054ff0f
Egor-oop <gulidoegor@gmail.com>
2022-09-21 17:30:11 +0300

    chores: 'user' field has the username of user

--- apps/postsapp/serializers.py
+++ apps/postsapp/serializers.py
@@ -4,6 +4,7 @@ from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(slug_field='username', read_only=True)
     class Meta:
         model = Post
         fields = '__all__'
